ec61e84b9fb58b42deeebf5ac818a3650688a92b
Li <453984016@qq.com>
2018-03-28 20:09:46 +0800

    后端框架基本初始化完成

---
+++ src/main/java/com/hypershark/ljfood/LjfoodApplication.java
@@ -0,0 +1,12 @@
+package com.hypershark.ljfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjfoodApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hypershark/ljfood/LjfoodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hypershark.ljfood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LjfoodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
